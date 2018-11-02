3ffe8d34c0a60b893f132729d90d5670d044364e
Wasif Khan <waasif.khaan@gmail.com>
2018-11-02 14:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function printBadges (names) {
-  for (let i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
-  }
+// Code your solutions in this file
 
-  return names;
+function printBadges(names){
+
+  for(let i = 0, i < names.length, i++){
+
+  console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+}
+return names;
 }
