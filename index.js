777eaf24e4c0ee3a4029ada64e8d4cf5449b1557
Wasif Khan <waasif.khaan@gmail.com>
2018-11-02 14:01:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
 const array =[];
 function printBadges(array){
-  binding.pry
-  for(let i = 0, i< array.length, i++){
+  
+  for(let i = 0, i < array.length, i++){
 
-    console.log(array[i])
+    console.log("${array[i]}")
 }
 return array;
 }
