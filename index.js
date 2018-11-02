0bc1f978b37880ed0c462543c7a9a1af50335beb
Wasif Khan <waasif.khaan@gmail.com>
2018-11-02 14:13:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function printBadges(array){
+
+  for(let i = 0; i < array.length; i++){
+
+  console.log(`Welcome ${array[i]}! You are employee #${i + 1}.`);
+}
+return array;
+}
+
+
+function tailsNeverFails(){
+  let i = 0;
+  while (Math.random() >= 0.5) {
+    i = i + 1;
+  }
+  return `You got ${i} tails in a row!`;
+}
